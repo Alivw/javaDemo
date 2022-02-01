fa0b51f75f972d4e9ad547109cb1f2073b7175f2
jalivv <shizuwei99@gmail.com>
2022-02-01 12:18:32 +0800

    wait/notify 导致程序永久挂起

--- SEProjects/src/thread/juc/demo1/Main.java
+++ SEProjects/src/thread/juc/demo1/Main.java
@@ -14,7 +14,8 @@ public class Main {
         //new Main().test1();
         //new Main().test2();
         //new Main().test3();
-        new Main().test4();
+        //new Main().test4();
+        new Main().test5();
 
 
     }
@@ -109,6 +110,43 @@ public class Main {
         Thread t1 = new Thread(() -> {
             if (ICE_CREAM == null) {
                 System.out.println("没有冰淇淋，小朋友不开心，等待。。。");
+                synchronized (this) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+
+            // 等待唤醒
+            // 被唤醒就代表冰淇凌做好了
+            System.out.println("小朋友买到冰淇凌");
+        });
+        t1.start();
+        Thread.sleep(2000L);
+        synchronized (this) {
+            this.notifyAll();
+        }
+        System.out.println("冰淇凌做好了，通知小朋友");
+    }
+
+
+    /**
+     * wait/notify 导致程序永久挂起
+     * notify 在前，wait 在后，导致程序永久挂起
+     */
+    public void test5() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            if (ICE_CREAM == null) {
+                System.out.println("没有冰淇淋，小朋友不开心，等待。。。");
+                try {
+                    Thread.sleep(6000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 synchronized (this) {
                     try {
                         wait();
