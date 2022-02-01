0f16c824423d7df262b8015fa8d4d2915070ca17
jalivv <shizuwei99@gmail.com>
2022-02-01 12:05:16 +0800

    suspend/resume 程序永久挂起

--- SEProjects/src/thread/juc/demo1/Main.java
+++ SEProjects/src/thread/juc/demo1/Main.java
@@ -11,7 +11,9 @@ public class Main {
     public static final Object ICE_CREAM = null;
 
     public static void main(String[] args) throws InterruptedException {
-        new Main().test2();
+        //new Main().test1();
+        //new Main().test2();
+        new Main().test3();
 
 
     }
@@ -42,6 +44,7 @@ public class Main {
      */
     public void test2() throws InterruptedException {
         Thread t1 = new Thread(() -> {
+            System.out.println(this);
             if (ICE_CREAM == null) {
                 System.out.println("没有冰淇淋，小朋友不开心，等待。。。");
                 synchronized (this) {
@@ -54,6 +57,7 @@ public class Main {
             System.out.println("小朋友买到冰淇凌");
         });
         t1.start();
+        System.out.println(this);
         Thread.sleep(2000L);
 
         synchronized (this) {
@@ -63,5 +67,33 @@ public class Main {
     }
 
 
+    /**
+     * 导致程序永久挂起 suspend/resume
+     * 小朋友 sleep 时间太长，由于 resume 的时候还没有执行 suspend ，造成程序永久挂起
+     *
+     * @throws InterruptedException
+     */
+    public void test3() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            if (ICE_CREAM == null) {
+                System.out.println("没有冰淇淋，小朋友不开心，等待。。。");
+                try {
+                    Thread.sleep(6000L);
+                    Thread.currentThread().suspend();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
+
+            }
+
+            // 等待唤醒
+            // 被唤醒就代表冰淇凌做好了
+            System.out.println("小朋友买到冰淇凌");
+        });
+        t1.start();
+        Thread.sleep(2000L);
+        t1.resume();
+        System.out.println("冰淇凌做好了，通知小朋友");
+    }
 }
