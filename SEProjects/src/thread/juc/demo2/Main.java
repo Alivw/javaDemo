0de38815973014b8d243720b38a99621f78ecd63
jalivv <shizuwei99@gmail.com>
2022-02-01 14:17:40 +0800

    lock 线程间通信

--- SEProjects/src/thread/juc/demo2/Main.java
+++ SEProjects/src/thread/juc/demo2/Main.java
@@ -1,5 +1,15 @@
 package thread.juc.demo2;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+interface share {
+    void incr() throws InterruptedException;
+
+    void desc() throws InterruptedException;
+}
+
 /**
  * @Description 尚硅谷线程间通信
  * @Created: with IntelliJ IDEA.
@@ -9,7 +19,9 @@ package thread.juc.demo2;
 public class Main {
     public static void main(String[] args) {
 
-        share share = new share();
+        //share share = new shareIf();
+        //share share = new shareWhile();
+        share share = new shareLock();
         new Thread(() -> {
             for (int i = 0; i < 10; i++) {
                 try {
@@ -30,16 +42,34 @@ public class Main {
                 }
             }
         }, "B").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    share.desc();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "C").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    share.desc();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "D").start();
+
     }
 }
 
-
-class share {
+class shareWhile implements share {
 
     private int number = 0;
 
     public synchronized void incr() throws InterruptedException {
-        while (number == 1) {
+        while (number != 0) {
             wait();
         }
 
@@ -58,4 +88,69 @@ class share {
         System.out.println(Thread.currentThread().getName() + "::" + number);
     }
 
+}
+
+class shareIf implements share {
+
+    private int number = 0;
+
+    public synchronized void incr() throws InterruptedException {
+        if (number != 0) {
+            wait();
+        }
+
+        number++;
+        notifyAll();
+        System.out.println(Thread.currentThread().getName() + "::" + number);
+    }
+
+    public synchronized void desc() throws InterruptedException {
+        if (number == 0) {
+            wait();
+        }
+
+        number--;
+        notifyAll();
+        System.out.println(Thread.currentThread().getName() + "::" + number);
+    }
+
+}
+
+class shareLock implements share {
+
+    private Lock lock = new ReentrantLock();
+
+    private Condition condition = lock.newCondition();
+
+    private int number = 0;
+
+    @Override
+    public void incr() throws InterruptedException {
+        try {
+            lock.lock();
+            while (number != 0) {
+                condition.await();
+            }
+            number++;
+            condition.signalAll();
+            System.out.println(Thread.currentThread().getName() + "::" + number);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public void desc() throws InterruptedException {
+        try {
+            lock.lock();
+            while (number != 1) {
+                condition.await();
+            }
+            number--;
+            condition.signalAll();
+            System.out.println(Thread.currentThread().getName() + "::" + number);
+        } finally {
+            lock.unlock();
+        }
+    }
 }
