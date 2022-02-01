55cb340be11d31cd1007e5d77466a4818bbb1cf7
jalivv <shizuwei99@gmail.com>
2022-02-01 12:16:05 +0800

    wait/notify 正常逻辑

--- SEProjects/src/thread/juc/demo1/Main.java
+++ SEProjects/src/thread/juc/demo1/Main.java
@@ -13,7 +13,8 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         //new Main().test1();
         //new Main().test2();
-        new Main().test3();
+        //new Main().test3();
+        new Main().test4();
 
 
     }
@@ -69,7 +70,7 @@ public class Main {
 
     /**
      * 导致程序永久挂起 suspend/resume
-     * 小朋友 sleep 时间太长，由于 resume 的时候还没有执行 suspend ，造成程序永久挂起
+     * 小朋友 sleep 时间太长，由于 resume 的时候还没有执行  suspend ，造成程序永久挂起
      *
      * @throws InterruptedException
      */
@@ -94,6 +95,40 @@ public class Main {
         t1.start();
         Thread.sleep(2000L);
         t1.resume();
+        System.out.println("冰淇凌做好了，通知小朋友");
+    }
+
+    /**
+     * wait 方法会释放持有的对象锁
+     * wait 方法必须放在同步代码块中，而且唤醒必须持有的是同一对象锁
+     * 正常 wait/notify
+     *
+     * @throws InterruptedException
+     */
+    public void test4() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            if (ICE_CREAM == null) {
+                System.out.println("没有冰淇淋，小朋友不开心，等待。。。");
+                synchronized (this) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+
+            // 等待唤醒
+            // 被唤醒就代表冰淇凌做好了
+            System.out.println("小朋友买到冰淇凌");
+        });
+        t1.start();
+        Thread.sleep(2000L);
+        synchronized (this) {
+            this.notifyAll();
+        }
         System.out.println("冰淇凌做好了，通知小朋友");
     }
 }
