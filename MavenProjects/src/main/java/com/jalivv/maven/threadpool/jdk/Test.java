e530597d0e6048161d724985404a200e15cd0881
jalivv <shizuwei99@gmail.com>
2022-02-10 11:08:26 +0800

    线程池 invokeAll

--- MavenProjects/src/main/java/com/jalivv/maven/threadpool/jdk/Test.java
+++ MavenProjects/src/main/java/com/jalivv/maven/threadpool/jdk/Test.java
@@ -2,6 +2,8 @@ package com.jalivv.maven.threadpool.jdk;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,7 +18,47 @@ import java.util.concurrent.Future;
 @Slf4j(topic = "c.Test")
 public class Test {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        test3(pool);
 
+    }
+
+    /**
+     * invokeAll方法
+     * @param pool
+     * @throws InterruptedException
+     */
+    private static void test3(ExecutorService pool) throws InterruptedException {
+        List<Future<Integer>> futures = pool.invokeAll(Arrays.asList(
+                () -> {
+                    log.debug("task1...");
+                    return 1;
+                },
+                () -> {
+                    log.debug("task2....");
+                    return 2;
+                },
+                () -> {
+                    log.debug("task3....");
+                    return 3;
+                }
+        ));
+        futures.forEach(item -> {
+            try {
+                log.debug("{}", item.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+    /**
+     * submit 方法
+     *
+     * @throws Exception
+     */
+    private static void test2() throws Exception {
         ExecutorService pool = Executors.newFixedThreadPool(4);
         Future<String> future = pool.submit(() -> {
             log.debug("开始执行....");
@@ -25,7 +67,6 @@ public class Test {
         });
 
         log.debug("{}", future.get());
-
     }
 
     /**
@@ -35,7 +76,7 @@ public class Test {
         ExecutorService pool = Executors.newFixedThreadPool(4);
         for (int i = 0; i < 10; i++) {
             int j = i;
-            pool.execute(()->{
+            pool.execute(() -> {
                 log.debug("....." + j);
             });
         }
