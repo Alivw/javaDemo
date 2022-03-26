29454826d453ff382e99502d7a4809a2eddd68ce
jalivv <shizuwei99@gmail.com>
2022-03-26 14:10:39 +0800

    Netty

--- NettyLearning/src/main/java/com/jalivv/netty/TestByteBuffer.java
+++ NettyLearning/src/main/java/com/jalivv/netty/TestByteBuffer.java
@@ -1,10 +1,10 @@
-
 package com.jalivv.netty;
 
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -12,12 +12,15 @@ import java.nio.channels.FileChannel;
 @Slf4j
 public class TestByteBuffer {
     public static void main(String[] args) {
+
+
         try (FileChannel channel = new FileInputStream("NettyLearning/data.txt").getChannel()) {
             ByteBuffer buffer = ByteBuffer.allocate(10);
             while (channel.read(buffer) != -1) {
                 buffer.flip();
                 while (buffer.hasRemaining()) {
                     log.debug("读取字节:{}", (char) buffer.get());
+                    System.out.println(buffer.get());
                 }
                 buffer.clear();
             }
