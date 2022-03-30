c72361252e365e4d0aeff6a5919eb78a8f9e7726
jalivv <shizuwei99@gmail.com>
2022-03-30 09:11:42 +0800

    模拟解析 @Bean

--- springboot-myh/src/main/java/com/jalivv/spring/a05/A05Application2.java
+++ springboot-myh/src/main/java/com/jalivv/spring/a05/A05Application2.java
@@ -12,20 +12,28 @@ import org.springframework.core.type.classreading.MetadataReader;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * 模拟解析 @Bean
+ */
 public class A05Application2 {
     public static void main(String[] args) throws IOException {
+        // 创建一个【干净】的容器
         GenericApplicationContext context = new GenericApplicationContext();
 
+        // 将 Config 对象 注册到容器中
         context.registerBean("config", Config.class);
 
+        // 获取读取类信息的reader
         CachingMetadataReaderFactory cachingMetadataReaderFactory = new CachingMetadataReaderFactory();
         MetadataReader reader = cachingMetadataReaderFactory.getMetadataReader(new ClassPathResource("com/jalivv/spring/a05/Config.class"));
+
+        // 从封装的 reader 中，读取到 被 @Bean 标记的方法
         for (MethodMetadata annotatedMethod : reader.getAnnotationMetadata().getAnnotatedMethods(Bean.class.getName())) {
             System.out.println(annotatedMethod);
-            //String initMethod = annotatedMethod.getAllAnnotationAttributes(Bean.class.getName()).get("initMethod").toString();
+            // 获取 初始化方法
             List<Object> initMethod = annotatedMethod.getAllAnnotationAttributes(Bean.class.getName()).get("initMethod");
             BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition();
-            // 那个对象(config)来调用这些方法 生成 bean
+            // 哪个对象(config)来调用这些方法 生成 bean
             builder.setFactoryMethodOnBean(annotatedMethod.getMethodName(), "config");
             // 指定装配模式 方法参数是需要装配的
             builder.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_CONSTRUCTOR);
@@ -34,6 +42,7 @@ public class A05Application2 {
                     builder.setInitMethodName((String) o);
                 }
             }
+            // 注册到 BeanFactory 中
             AbstractBeanDefinition beanDefinition = builder.getBeanDefinition();
             context.getDefaultListableBeanFactory().registerBeanDefinition(annotatedMethod.getMethodName(),beanDefinition);
         }
---
+++ springboot-myh/src/main/java/com/jalivv/spring/a05/mapper/Mepper3.java
@@ -0,0 +1,9 @@
+package com.jalivv.spring.a05.mapper;
+
+/**
+ * @Description TODO
+ * @Date 2022/3/30 09:07
+ * @Created by jalivv
+ */
+public interface Mepper3 {
+}
